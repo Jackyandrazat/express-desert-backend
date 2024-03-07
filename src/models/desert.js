f1bf2fced7aba4d93bdb9149760fdacb0d3862cd
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2024-03-07 12:07:11 +0700

    Update desert.js
    added validasi  minus data

--- src/models/desert.js
+++ src/models/desert.js
@@ -5,6 +5,9 @@ const getAllDesert = async () => {
     return dbPool.execute(sqlQuery);
 }
 const createNewDesert = async (body) => {
+    if (body.stock < 0 || body.harga < 0) {
+        throw new Error("Stock dan harga tidak boleh negatif.");
+    }
     const sqlQuery = `INSERT INTO deserts (nama, stock, harga) 
                       VALUES (?,?,?)`;
     const values = [body.nama,body.stock,body.harga];
@@ -13,6 +16,9 @@ const createNewDesert = async (body) => {
 }
 
 const updateDesert = async (body, id) => {
+    if (body.stock < 0 || body.harga < 0) {
+        throw new Error("Stock dan harga tidak boleh negatif.");
+    }
     const sqlQuery = `UPDATE deserts 
                         SET nama = '${body.nama}', stock = '${body.stock}', harga = '${body.harga}' 
                         WHERE id = ${id}`
